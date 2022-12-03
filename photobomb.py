43b92e4f904fb5c860cd4781d3bf50b6a5317774
Ronav Gupta <guptaronav8@gmail.com>
2022-12-03 14:21:10 -0800

    choose_places_and_victim
    places and victims are WIP

--- photobomb.py
+++ photobomb.py
@@ -4,22 +4,68 @@ import sys
 import pygame
 import math, time, random
 import random
+from enum import Enum
 from pygame.locals import *
 pygame.display.set_caption("Photobomb")
 WIDTH = 1500
 HEIGHT = 900
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 
+class Places(Enum):
+    Hawaii, France, India = 1,2,3
+class Victims(Enum):
+    Shubham, Robert, Pronamee  = 1,2,3
 class Photobomb:
     def __init__(self):
         self.buffer=100
-        self.startbg=pygame.image.load(r'startingbg.jpg').convert_alpha()
+        self.firstbg=pygame.image.load(r'firstbg.jpg').convert_alpha()
+        self.secondbg=pygame.image.load(r'secondbg.jpeg').convert_alpha()
+        self.chalkFont = pygame.font.Font('font/Chalkduster.ttf', 50)
+        self.chalkFont2 = pygame.font.Font('font/Chalkduster.ttf', 22)
+        self.chalkFont3 = pygame.font.Font('font/Chalkduster.ttf', 35)
         self.font = pygame.font.Font('font/CoffeeTin.ttf', 150)
         self.font2 = pygame.font.Font('font/IndianPoker.ttf', 75)
         self.font2.set_bold(True)
         self.startText = self.font2.render("Ready to Photobomb?", 1, (randcol()))
         self.startSize = self.font2.size("Ready to Photobomb?")
+        
+        self.placeText = self.chalkFont3.render("Choose your location: ", 1, (green))
+        self.placeSize = self.chalkFont3.size("Choose your location: ")
+        
+        self.place1 = self.chalkFont3.render(Places.Hawaii.name, 1, pygame.Color(cyan))
+        self.place1Size = self.chalkFont3.size(Places.Hawaii.name)
+        self.place1Loc = (700, self.buffer + 100)
+        self.place1Rect = pygame.Rect((self.place1Loc[0]-10,self.place1Loc[0]),(self.place1Size[0]+10,self.place1Size[1]))
+        
+        self.place2 = self.chalkFont3.render(Places.France.name, 1, pygame.Color(cyan))
+        self.place2Size = self.chalkFont3.size(Places.Hawaii.name)
+        self.place2Loc = (900, self.buffer + 100)
+        self.place2Rect = pygame.Rect((self.place2Loc[0]-10,self.place2Loc[0]),(self.place2Size[0]+10,self.place2Size[1]))
+        
+        self.place3 = self.chalkFont3.render(Places.India.name, 1, pygame.Color(cyan))
+        self.place3Size = self.chalkFont3.size(Places.Hawaii.name)
+        self.place3Loc = (1100, self.buffer + 100)
+        self.place3Rect = pygame.Rect((self.place3Loc[0]-10,self.place3Loc[0]),(self.place3Size[0]+10,self.place3Size[1]))
+        
+        self.vicText = self.chalkFont3.render("Choose your victim: ", 1, (green))
+        self.vicSize = self.chalkFont3.size("Choose your victim: ")
+        
+        self.vic1 = self.chalkFont3.render(Victims.Shubham.name, 1, pygame.Color(cyan))
+        self.vic1Size = self.chalkFont3.size(Victims.Shubham.name)
+        self.vic1Loc = (650, self.buffer + 200)
+        self.vic1Rect = pygame.Rect((self.vic1Loc[0]-10,self.vic1Loc[0]),(self.vic1Size[0]+10,self.vic1Size[1]))
+        
+        self.vic2 = self.chalkFont3.render(Victims.Robert.name, 1, pygame.Color(cyan))
+        self.vic2Size = self.chalkFont3.size(Victims.Robert.name)
+        self.vic2Loc = (900, self.buffer + 200)
+        self.vic2Rect = pygame.Rect((self.vic2Loc[0]-10,self.vic2Loc[0]),(self.vic2Size[0]+10,self.vic2Size[1]))
+        
+        self.vic3 = self.chalkFont3.render(Victims.Pronamee.name, 1, pygame.Color(cyan))
+        self.vic3Size = self.chalkFont3.size(Victims.Pronamee.name)
+        self.vic3Loc = (1100, self.buffer + 200)
+        self.vic3Rect = pygame.Rect((self.vic3Loc[0]-10,self.vic3Loc[0]),(self.vic3Size[0]+10,self.vic3Size[1]))
         self.start_up_init()
+        
 
     def show_text(self, msg, x, y, color, size):
         tin = pygame.font.Font('font/IndianPoker.ttf', size)
@@ -35,13 +81,22 @@ class Photobomb:
         self.startButtonRect = pygame.Rect(self.startButtonLoc, self.startButtonSize)
         self.startButtonRectOutline = pygame.Rect(self.startButtonLoc, self.startButtonSize)
         self.startLoc = (WIDTH/2 - self.startSize[0]/2, self.buffer)
+        
+        self.playButton = self.font2.render(" Play ", 1, black)
+        self.playButtonSize = self.font2.size(" Play ")
+        self.playButtonLoc = (WIDTH/2 - self.playButtonSize[0]/2, HEIGHT/2 - self.playButtonSize[1]/2)
+        self.playButtonRect = pygame.Rect(self.playButtonLoc, self.playButtonSize)
+        self.playButtonRectOutline = pygame.Rect(self.playButtonLoc, self.playButtonSize)
+
+        self.selectedPlace=Places.Hawaii
+        self.selectedVictim=Victims.Shubham
         self.state = 0
 
     def main(self):
         if self.state == 0:
             self.show_splash_screen()
         elif self.state == 1:
-            self.play()
+            self.select_loc_and_victim()
         elif self.state == 2:
              self.results()
 
@@ -63,9 +118,8 @@ class Photobomb:
                     mouseRect = pygame.Rect(event.pos, (1, 1))
                     if mouseRect.colliderect(self.startButtonRect):
                         self.state = 1
-                        self.play_init()
                         return
-            SCREEN.blit(self.startbg,(0,0))
+            SCREEN.blit(self.firstbg,(0,0))
             # draw the start button
             pygame.draw.rect(SCREEN, green, self.startButtonRect)
             pygame.draw.rect(SCREEN, black, self.startButtonRectOutline, 2)
@@ -73,10 +127,64 @@ class Photobomb:
             SCREEN.blit(self.startText, self.startLoc)
             pygame.display.flip()
 
-    def play_init(self):
-        # create the new variables
-        self.round = 0
+    def select_loc_and_victim(self):
+        global SCREEN, WIDTH, HEIGHT
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.VIDEORESIZE:
+                SCREEN = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+                WIDTH = event.w
+                HEIGHT = event.h
+            # when the user chooses the info change to the playing state
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if self.place1Rect.collidepoint(event.pos):
+                        selectedPlace=Places.Hawaii
+                    if self.place2Rect.collidepoint(event.pos):
+                        selectedPlace=Places.France
+                    if self.place3Rect.collidepoint(event.pos):
+                        selectedPlace=Places.India
+                    if self.vic1Rect.collidepoint(event.pos):
+                        selectedVictim=Victims.Shubham
+                    if self.vic2Rect.collidepoint(event.pos):
+                        selectedVictim=Victims.Robert
+                    if self.vic3Rect.collidepoint(event.pos):
+                        selectedVictim=Victims.Pronamee
+                    if self.playButtonRect.collidepoint(event.pos):
+                        self.state=2
         
+        SCREEN.blit(self.secondbg,(0,0))
+        # draw the start button
+        #pygame.draw.rect(SCREEN, green, self.startButtonRect)
+        #pygame.draw.rect(SCREEN, black, self.startButtonRectOutline, 2)
+        #SCREEN.blit(self.startButton, self.startButtonLoc)
+        #SCREEN.blit(self.startText, self.startLoc)
+        SCREEN.blit(self.placeText,(200,self.place1Loc[1]))
+        SCREEN.blit(self.place1,self.place1Loc)
+        pygame.draw.circle(SCREEN, white, (self.place1Loc[0] - 20, self.place1Loc[1] + 22), 16, 2)
+        SCREEN.blit(self.place2,self.place2Loc)
+        pygame.draw.circle(SCREEN, white, (self.place2Loc[0] - 20, self.place2Loc[1] + 22), 16, 2)
+        SCREEN.blit(self.place3,self.place3Loc)
+        pygame.draw.circle(SCREEN, white, (self.place3Loc[0] - 20, self.place3Loc[1] + 22), 16, 2)
+        
+        SCREEN.blit(self.vicText,(200,self.vic1Loc[1]))
+        SCREEN.blit(self.vic1,self.vic1Loc)
+        pygame.draw.circle(SCREEN, white, (self.vic1Loc[0] - 20, self.vic1Loc[1] + 22), 16, 2)
+        SCREEN.blit(self.vic2,self.vic2Loc)
+        pygame.draw.circle(SCREEN, white, (self.vic2Loc[0] - 20, self.vic2Loc[1] + 22), 16, 2)
+        SCREEN.blit(self.vic3,self.vic3Loc)
+        pygame.draw.circle(SCREEN, white, (self.vic3Loc[0] - 20, self.vic3Loc[1] + 22), 16, 2)
+        
+        pygame.draw.rect(SCREEN, green, self.playButtonRect)
+        pygame.draw.rect(SCREEN, black, self.playButtonRectOutline, 2)
+        SCREEN.blit(self.playButton, self.playButtonLoc)
+            
+        pygame.display.flip()
+            
+    
 #############################################################
 if __name__ == "__main__":
     os.environ['SDL_VIDEO_CENTERED'] = '1'  # center SCREEN
